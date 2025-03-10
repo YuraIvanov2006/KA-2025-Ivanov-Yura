d473c9e3fa660a52103aee6b229cc9e09120cd05
Yura <yu.ivanov@ukma.edu.ua>
2025-03-10 13:23:13 +0200

    Update

--- Labs/Lab04/task01a.asm
+++ Labs/Lab04/task01a.asm
@@ -1,7 +1,7 @@
 .model small
 .stack 100h
 .data 
-msg db 14 dup(0AAh) 
+msg dw 14 dup(0) 
 
 .code
 main PROC
@@ -10,17 +10,17 @@ main PROC
 
     mov dx, offset msg
  
-    mov al, 0; counter 
-    mov bh, 3   
-    cmp al, 14
+    mov si, 0; counter 
+    mov bx, 3   
+    cmp si, 14
     jl array_set
     
 
     array_set:
-    add bh, 3
-    mov [msg + al], bh
-    inc al
-    cmp al, 14
+    add bx, 3
+    mov [msg + si], bx
+    inc si
+    cmp si, 14
     jl array_set
 
     
